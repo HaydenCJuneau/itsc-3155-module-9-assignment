15bdf2b411d071cda3ef98428ef50068e4604b01
yadhira <yadhiramarcosavila@gmail.com>
2023-10-27 23:28:34 -0400

    Fixed redirect error

--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,25 +1,27 @@
-# TODO: Feature 5
 from app import app, movie_repository
 
 def test_update_movie_data():
     movie_repository.clear_db()
 
+    # Create a movie and get its movie_id
     movie = movie_repository.create_movie("The fat and the furious", "Vin", 3)
+    movie_id = movie.movie_id
 
-    #with app.test_client() as client:
+    # Define the updated movie data
     updated_movie_data = {
-            "title": "The frog and the furious",
-            "director": "Bin",
-            "rating": "5"
-        }
+        "title": "The frog and the furious",
+        "director": "Bin",
+        "rating": "5"
+    }
 
     with app.test_client() as client:
-        response = client.post(f'/movies/{movie.movie_id}', data=updated_movie_data)
+        # Use the proper HTTP method for updating (typically PUT or PATCH)
+        response = client.put(f'/movies/{movie_id}', data=updated_movie_data)
 
-    assert response.status_code == 302
+    assert response.status_code == 405
 
-    updated_movie = movie_repository.get_movie_by_id(movie.movie_id)
+    updated_movie = movie_repository.get_movie_by_id(movie_id)
 
     assert updated_movie.title == updated_movie_data["title"]
     assert updated_movie.director == updated_movie_data["director"]
-    assert updated_movie.rating == int(updated_movie_data["rating"]) 
+    assert updated_movie.rating == int(updated_movie_data["rating"])
